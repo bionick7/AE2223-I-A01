e34ba25857547c3e7bac081c9da5e115047b5ce5
bionick7 <42216907+bionick7@users.noreply.github.com>
2022-02-24 16:32:05 +0100

    First file create
    Just a main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pylplot as plt
+
+"""
+Main file of the project. Keep it short and export bulk work to other files if it grows over, say,  100 lines
+"""
+
+def main():
+  pass
+
+if __name__ == "__main__":
+  main()
